a8fd791ffe7c464bdb6e70c1e0e9452777c5b3cd
ajdin <ajdinhusetovic@outlook.com>
2023-01-07 13:24:32 +0100

    testing more functionality and added functionality to the clear button so it clears the input field

--- app.js
+++ app.js
@@ -1,9 +1,37 @@
 const buttons = document.querySelectorAll('.btn');
 const input = document.querySelector('.inpt');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('.clr');
+
+const clearInputField = () => {
+    input.value = " ";
+    console.log(input.value)
+}
+
+const timesTwo = (number) => {
+    input.value = number*2;
+    console.log(input.value)
+}
 
 buttons.forEach(btn => {
     btn.addEventListener('click', () => {
-        input.value = btn.textContent;
+        input.value = btn.dataset.num
+      
     })
 })
+
+
+equal.addEventListener('click', (e)=>{
+    e.preventDefault();
+    timesTwo(input.value)
+})
+
+clear.addEventListener('click', (e) => {
+    e.preventDefault();
+    clearInputField();
+});
+
+
+
+
 
